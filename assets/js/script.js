afa9dd653510d71fb3ecf2d5b70bd57a2caacc08
mikedaleo <mikedaleo@outlook.com>
2024-04-24 17:17:30 -0400

    accidentally worked on main

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,46 @@
 const APIkey = "eaca71861ed45fe78b17a37097ba2475";
-const searchHistory = document.querySelector('#searchHistory');
 const today = document.querySelector('#today');
 const forecast = document.querySelector('#forecast');
-const searchFormEl = document.querySelector('#searchForm');
-let cityInputEl = document.querySelector('#cityInput');
-const searchButton = document.querySelector('#searchButton')
+const searchFormEl = document.querySelector('#search-form');
+let cityInputEl = document.querySelector('#city-input');
+const searchButton = document.querySelector('#search-button')
 dayjs().format()
+const errorMessageDiv = document.createElement('div');
+const weatherDiv = document.querySelector('#weather');
+const todayWeather = document.querySelector('#today');
+const weeklyForecast = document.querySelector('#forecast');
+const fiveDayHeader = document.querySelector('#five-day');
 
+// save search history to localStorage
+function saveSearchHistory(cityToSave) {
+    let storedSearches = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    let index = storedSearches.indexOf(cityToSave);
+    if (index !== -1) {
+        storedSearches.splice(index, 1);
+    };
+    storedSearches.push(cityToSave);
+    if (storedSearches.length > 10) {
+        storedSearches.splice(0, storedSearches.length - 10)
+    }
+    localStorage.setItem('searchHistory', JSON.stringify(storedSearches));
+}
 
+// load search history from localStorage
+// function loadSearchHistory() {
+//     const searchHistoryDiv = document.querySelector('#search-history');
+//     let searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
+//     searchHistoryDiv.innerHTML = '';
+//     searchHistory.forEach(search => {
+//         const historyBtn = document.createElement('button');
+//         historyBtn.textContent = search;
+//         historyBtn.setAttribute('class', 'search-history');
+//         searchHistoryDiv.append(historyBtn);
+//         historyBtn.addEventListener('click', function() {
+//             cityInputEl.value = search;
+//             convertCityNameToCoords(search);
+//         })
+//     })
+// }
 
 
 // geocoding API - converting zip code to coordinates
@@ -36,8 +69,17 @@ function convertCityNameToCoords(city) {
             let cityURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${APIkey}`
             // get weather forecast of cityURL;
             getWeatherData(cityURL);
+            todayWeather.innerHTML = '';
+            weeklyForecast.innerHTML = '';
+            fiveDayHeader.innerHTML = '';
+            errorMessageDiv.innerHTML = '';
         })
         .catch(function (error) {
+            errorMessageDiv.innerHTML = '';
+            errorMessageDiv.setAttribute('class', 'error-message');
+            errorMessageDiv.textContent = 'Please enter a valid city name.';
+            searchFormEl.append(errorMessageDiv);
+            // alert('Please enter a valid city name.')
             console.log(error);
         })
 };
@@ -53,27 +95,24 @@ function getWeatherData(cityURL) {
             }
             return response.json();
         })
-        .then(function (cityForecast) {
-            console.log(cityForecast);
-            console.log(cityForecast.city.name);
-            console.log(cityForecast.list[0])
-            console.log(`Temp: ${cityForecast.list[0].main.temp}`);
-            console.log(`Wind: ${cityForecast.list[0].wind.speed}`);
-            console.log(`Humidity: ${cityForecast.list[0].main.humidity}%`);
-            createWeatherCard(cityForecast);
+        .then(function (data) {
+            // console.log(data);
+            // console.log(data.city.name);
+            // console.log(data.list[0])
+            // console.log(`Temp: ${data.list[0].main.temp}`);
+            // console.log(`Wind: ${data.list[0].wind.speed}`);
+            // console.log(`Humidity: ${data.list[0].main.humidity}%`);
+            createWeatherCard(data);
+            saveSearchHistory(data.city.name);
         })
 
 }
 
 // create a card with the day's weather information for each day
 function createWeatherCard(cityForecast) {
-    const todayWeather = document.querySelector('#today');
-    const weeklyForecast = document.querySelector('#forecast');
-    const forecastHeader = document.createElement('h3');
-    forecastHeader.textContent = '5-Day Forecast:'
-    weeklyForecast.append(forecastHeader);
+    fiveDayHeader.textContent = '5-Day Forecast:'
 
-    for (let i = 0; i < 40; i += 8) {
+    for (let i = 0; i < 40; i += 7) {
         const card = document.createElement('div');
         const cardDate = document.createElement('h3');
         const cardTemp = document.createElement('p');
@@ -93,13 +132,11 @@ function createWeatherCard(cityForecast) {
         cardWind.textContent = `Wind: ${wind}`;
         cardHumidity.textContent = `Humidity: ${humidity}%`;
         card.append(cardDate, weatherIconImg, cardTemp, cardWind, cardHumidity);
-        // todayWeather.append(card);
         if (i === 0) {
             cardDate.textContent = `${cityName} (${date})`;
-            cardDate.append(weatherIconImg);
             todayWeather.append(card);
             card.setAttribute('class', 'today');
-            
+
         } else {
             card.setAttribute('class', 'daily');
             console.log(`Date: ${date}`);
@@ -109,49 +146,19 @@ function createWeatherCard(cityForecast) {
             weeklyForecast.append(card);
         }
 
-        console.log(`Date: ${date}`);
-        console.log(`Temp: ${dailyCityForecast.main.temp}`);
-        console.log(`Wind: ${dailyCityForecast.wind.speed}`);
-        console.log(`Humidity: ${dailyCityForecast.main.humidity}%`);
+        // console.log(`Date: ${date}`);
+        // console.log(`Temp: ${dailyCityForecast.main.temp}`);
+        // console.log(`Wind: ${dailyCityForecast.wind.speed}`);
+        // console.log(`Humidity: ${dailyCityForecast.main.humidity}%`);
         cityInputEl.value = '';
     }
 }
 
-
-
-// function searchForecast(city) {
-//     let cityURL = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&units=imperial&appid=${APIkey}`;
-
-//     fetch(cityURL)
-//         .then(function (response) {
-//             if (!response.ok) {
-//                 throw response.json();
-//             }
-//             return response.json();
-//         })
-//         .then(function (cityForecast){
-//             console.log(cityForecast);
-//             console.log(cityForecast.city.name);
-//             console.log(cityForecast.list[0])
-//             console.log(`Temp: ${cityForecast.list[0].main.temp}`);
-//             console.log(`Wind: ${cityForecast.list[0].wind.speed}`);
-//             console.log(`Humidity: ${cityForecast.list[0].main.humidity}%`);
-//         })
-
-// }
-
 function handleFormSubmit(event) {
     event.preventDefault();
     let city = cityInputEl.value.trim();
-    // searchForecast(city);
     convertCityNameToCoords(city);
 };
 
-// get parameters of the query search 
-function getParams() {
 
-}
-
-searchButton.addEventListener('click', handleFormSubmit);
-
-// `http://api.openweathermap.org/data/2.5/weather?q=New-York&limit=5&appid=eaca71861ed45fe78b17a37097ba2475`
+searchFormEl.addEventListener('submit', handleFormSubmit);
